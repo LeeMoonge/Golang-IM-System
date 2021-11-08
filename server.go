3b79b7cd5f50d38f5adcff255c845735320778f9
limengjian <limengjian@51yund.com>
2021-11-08 16:13:13 +0800

    feat(master): 用户消息及广播功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -67,6 +68,28 @@ func (s *Server) Handle(conn net.Conn)  {
 
 	//2. 广播当前用户上线消息
 	s.BroadCast(user, "上线了")
+
+	//3. 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				s.BroadCast(user, "已下线！")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read Error :", err)
+				return
+			}
+
+			//3. 提取用户的消息(去除'\n')
+			msg := string(buf[:n-1])
+
+			//3. 将得到的消息进行广播
+			s.BroadCast(user, msg)
+		}
+	}()
 
 	//2. 当前handle阻塞
 	select {}
